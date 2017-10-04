4ba1a0519c88f51d263a17680e7ce3a77d526f11
silasq <30379895+silasq@users.noreply.github.com>
2017-10-04 15:58:53 +0800

    a

--- ssh_root.sh
+++ ssh_root.sh
@@ -1,3 +1,10 @@
+# change root password
 echo "passwd:"
 read pass
 echo $pass|passwd --stdin root
+
+# permit root login
+sed -i "s/PermitRootLogin no/PermitRootLogin yes/" /etc/ssd/ssd_config
+sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssd/ssd_config
+sed -i "s/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/" /etc/ssd/ssd_config
+service sshd restart
