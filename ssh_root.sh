cdeee52535f65d00fb60ed18481dba36f4bbf22f
silasq <30379895+silasq@users.noreply.github.com>
2017-10-04 16:00:54 +0800

    a

--- ssh_root.sh
+++ ssh_root.sh
@@ -4,7 +4,7 @@ read pass
 echo $pass|passwd --stdin root
 
 # permit root login
-sed -i "s/PermitRootLogin no/PermitRootLogin yes/" /etc/ssd/ssd_config
-sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssd/ssd_config
-sed -i "s/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/" /etc/ssd/ssd_config
+sed -i "s/PermitRootLogin no/PermitRootLogin yes/" /etc/ssh/ssh_config
+sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/ssh_config
+sed -i "s/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/" /etc/ssh/ssh_config
 service sshd restart
