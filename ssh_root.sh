e757f5c3da7dbebff98f765ed2ef9cbb724137d1
silasq <30379895+silasq@users.noreply.github.com>
2017-10-04 21:33:57 +0800

    update

--- ssh_root.sh
+++ ssh_root.sh
@@ -4,7 +4,7 @@ read pass
 echo $pass|passwd --stdin root
 
 # permit root login
-sed -i "s/PermitRootLogin no/PermitRootLogin yes/" /etc/ssh/ssh_config
-sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/ssh_config
-sed -i "s/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/" /etc/ssh/ssh_config
+sed -i "s/PermitRootLogin no/PermitRootLogin yes/" /etc/ssh/sshd_config
+sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
+sed -i "s/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/" /etc/ssh/sshd_config
 service sshd restart
